7bee4e710d7ef725f216d2e5a21137b0a8882094
kouriat <kouriat11mohamed@gmail.com>
2025-06-19 09:45:06 +0200

    init: products-page

---
+++ frontend/app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductsPage = () => {
+    return (
+        <main>
+            <h1>Products</h1>
+            <p>Welcome to the products page.</p>
+        </main>
+    );
+};
+
+export default ProductsPage;
