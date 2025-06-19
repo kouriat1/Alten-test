18d39dcb6b391248771ef4cbde38c940e0dd8dfb
kouriat <kouriat11mohamed@gmail.com>
2025-06-19 09:55:42 +0200

    feat: add products list

--- frontend/app/products/page.tsx
+++ frontend/app/products/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { Product } from '@/app/models/products/Product';
 
 const ProductsPage = () => {
+      const [products, setProducts] = useState<Product[]>([]);
+
+
+    useEffect(() => {
+    fetch('/products.json')
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
     return (
-        <main>
-            <h1>Products</h1>
-            <p>Welcome to the products page.</p>
-        </main>
+        <div>
+            <h1>Products LIST</h1>
+         <div className="flex-row">
+            {products.map((product) => (
+                <div key={product.id} className="border p-4 m-2 rounded shadow">
+                    <h2 className="text-xl font-semibold">{product.name}</h2>
+                    <p className="text-gray-700">{product.description}</p>
+                    <p className="text-lg font-bold text-pink-600">${product.price.toFixed(2)}</p>
+                </div>
+            ))}
+           </div>
+            
+        </div>
     );
 };
 
