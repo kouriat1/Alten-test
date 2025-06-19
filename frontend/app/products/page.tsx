16c8fe3f0b61bd098d7dc06dce2c1abaa6dd64b5
kouriat <kouriat11mohamed@gmail.com>
2025-06-19 09:59:50 +0200

    feat: add buttons

--- frontend/app/products/page.tsx
+++ frontend/app/products/page.tsx
@@ -5,6 +5,9 @@ import { Product } from '@/app/models/products/Product';
 const ProductsPage = () => {
       const [products, setProducts] = useState<Product[]>([]);
 
+  const handleDelete = (productId: number) => {
+    setProducts((prev) => prev.filter((p) => p.id !== productId));
+  };
 
     useEffect(() => {
     fetch('/products.json')
@@ -13,13 +16,24 @@ const ProductsPage = () => {
   }, []);
     return (
         <div>
-            <h1>Products LIST</h1>
+        <h1 className="text-2xl flex font-bold mb-4 justify-center items-center">Liste des produits</h1>
          <div className="flex-row">
             {products.map((product) => (
                 <div key={product.id} className="border p-4 m-2 rounded shadow">
                     <h2 className="text-xl font-semibold">{product.name}</h2>
                     <p className="text-gray-700">{product.description}</p>
                     <p className="text-lg font-bold text-pink-600">${product.price.toFixed(2)}</p>
+                    <div className="flex gap-2 mt-4">
+                <button className="bg-green-500 text-white px-3 py-1 rounded hover:bg-yellow-600">
+                  Modifier
+                </button>
+                <button
+                  onClick={() => handleDelete(product.id)}
+                  className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
+                >
+                  Supprimer
+                </button>
+              </div>
                 </div>
             ))}
            </div>
